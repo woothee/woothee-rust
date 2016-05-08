77a6a1a2db2df5f76a788741af20e79e6ffd0b61
hhatto <hhatto.jp@gmail.com>
2016-05-09 00:20:59 +0900

    v0.1.1

--- implementors/core/default/trait.Default.js
+++ implementors/core/default/trait.Default.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['woothee'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='woothee/parser/struct.WootheeResult.html' title='woothee::parser::WootheeResult'>WootheeResult</a>",];implementors['woothee'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='woothee/parser/struct.WootheeResult.html' title='woothee::parser::WootheeResult'>WootheeResult</a>",];
+implementors['woothee'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='woothee/parser/struct.WootheeResult.html' title='woothee::parser::WootheeResult'>WootheeResult</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
