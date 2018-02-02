aee5e173e86a65c825bc73dd555a45d29efb0db8
Hideo Hattori <hattori-h@klab.com>
2018-02-02 14:38:05 +0900

    add unit test (for #5)

--- src/lib.rs
+++ src/lib.rs
@@ -46,9 +46,25 @@ pub fn is_crawler(agent: &str) -> bool {
     parser.try_crawler(agent, &mut result)
 }
 
-#[test]
-fn test_is_crawler_smoke() {
-    assert!(!is_crawler("Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)"));
-    assert!(is_crawler("Mozilla/5.0 (compatible; Yahoo! Slurp; \
-                        http://help.yahoo.com/help/us/ysearch/slurp)"));
+#[cfg(test)]
+mod tests {
+    use super::{is_crawler, WootheeResult, Parser};
+
+    fn get_woothee_result(agent: &str) -> WootheeResult {
+        Parser::new().parse(agent).expect("fail parse()")
+    }
+
+    #[test]
+    fn test_parser_lifetime() {
+        let agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:21.0) Gecko/20100101 Firefox/21.0";
+        let result = get_woothee_result(agent);
+        assert_eq!(result.name.as_str(), "Firefox");
+    }
+
+    #[test]
+    fn test_is_crawler_smoke() {
+        assert!(!is_crawler("Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)"));
+        assert!(is_crawler("Mozilla/5.0 (compatible; Yahoo! Slurp; \
+                            http://help.yahoo.com/help/us/ysearch/slurp)"));
+    }
 }
