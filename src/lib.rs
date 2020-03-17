22408b8e6d77c5980be1c88b227b625ffc895785
Hideo Hattori <hhatto.jp@gmail.com>
2020-03-18 01:40:31 +0900

    fix doctest

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,7 @@
 //! ```
 //!
 //! ```rust
+//! use woothee::parser::Parser;
 //! let parser = Parser::new();
 //! let result = parser.parse("Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)");
 //! println!("{:?}", result);
