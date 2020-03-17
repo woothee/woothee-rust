514134962be0171a8962054f494422078e7a4f42
Hideo Hattori <hhatto.jp@gmail.com>
2020-03-18 01:22:33 +0900

    strict clippy

--- src/lib.rs
+++ src/lib.rs
@@ -11,15 +11,9 @@
 //! ```
 //!
 //! ```rust
-//! extern crate woothee;
-//!
-//! use woothee::parser::Parser;
-//!
-//! fn main() {
-//!     let parser = Parser::new();
-//!     let result = parser.parse("Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)");
-//!     println!("{:?}", result);
-//! }
+//! let parser = Parser::new();
+//! let result = parser.parse("Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)");
+//! println!("{:?}", result);
 //! ```
 //!
 
